cfb88a3097e48e3bf6e58f30226b635a2897928b
Shishir <shishir@Ss-MacBook-Air.local>
2024-11-10 13:52:14 +1100

    Inital set up

---
+++ apps/users/src/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: '123',
+      email: 'asdf',
+      password: 'sdf',
+    },
+  ];
+
+  create(createUserInput: CreateUserInput) {
+    this.users.push(createUserInput);
+    return createUserInput;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  // update(id: number, updateUserInput: UpdateUserInput) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
